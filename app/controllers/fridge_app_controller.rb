373213f28fb4b18ce5d6b5528053b126cf427ba6
MonicaGrages <mgrages1@gmail.com>
2017-06-21 21:35:40 -0400

    added links to fridge show page, but no content there yet. also added link to new fridge form but nothing there yet.

--- app/controllers/fridge_app_controller.rb
+++ app/controllers/fridge_app_controller.rb
@@ -2,4 +2,22 @@ class FridgeAppController < ApplicationController
   def index
     @fridges = Fridge.all
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'fridge_app/new'
+
+  get 'fridge_app/edit'
+
+  get 'fridge_app/create'
+
+  get 'fridge_app/update'
+
+  get 'fridge_app/destroy'
+
+  get 'fridge_app/show'
+
   get 'fridge_app/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
